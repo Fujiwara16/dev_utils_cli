a85a56af4553d6d777e862bb4cac856e821e0be5
nijmehar <nijmehar16@gmail.com>
2024-08-25 01:59:41 +0530

    remove main invokation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=open('requirements.txt').read().splitlines(),
     entry_points={
         'console_scripts': [
-            'xyz=cli:main',  # This creates the `xyz` command
+            'xyz=utils.cli',  # This creates the `xyz` command
         ],
     },
     author="Fujiwara",
@@ -23,4 +23,3 @@ setup(
     ],
     python_requires='>=3.6',
 )
-
