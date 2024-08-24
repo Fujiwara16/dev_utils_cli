f79a5b755f96f0fc568c1921f21712c5e13bc88a
nijmehar <nijmehar16@gmail.com>
2024-08-25 01:48:44 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dev_utils_cli",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    entry_points={
+        'console_scripts': [
+            'xyz=cli:main',  # This creates the `xyz` command
+        ],
+    },
+    author="Fujiwara",
+    author_email="nijmehar16@gmail.com",
+    description="A CLI app for developer utils",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Fujiwara16/dev_utils_cli",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
