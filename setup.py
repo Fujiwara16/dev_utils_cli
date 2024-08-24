b596cc785a340fa9c58bae6206ba97fa31b2e1b7
nijmehar <nijmehar16@gmail.com>
2024-08-25 02:02:06 +0530

    test.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=open('requirements.txt').read().splitlines(),
     entry_points={
         'console_scripts': [
-            'xyz=utils.cli',  # This creates the `xyz` command
+            'xyz=utils.cli:main',  # This creates the `xyz` command
         ],
     },
     author="Fujiwara",
