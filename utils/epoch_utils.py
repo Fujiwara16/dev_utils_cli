e91e01ba2f137d1c5b4532ac75048b0a79734fd3
nijmehar <nijmehar16@gmail.com>
2024-08-25 15:01:47 +0530

    timezone fix in datetime

--- utils/epoch_utils.py
+++ utils/epoch_utils.py
@@ -1,15 +1,15 @@
 from datetime import datetime
-from dateutil.tz import gettz
+import pytz
 
 
 def epoch_to_datetime(epoch, timezone='GMT'):
     try:
-        # Convert epoch to datetime
-        # localise the datetime object to the timezone
-
+        # Convert epoch to naive datetime (no timezone info)
         dt = datetime.fromtimestamp(int(epoch))
-        local_timezone = gettz(timezone)
-        localized_datetime = dt.replace(tzinfo=local_timezone)
+        # Set the desired timezone
+        target_timezone = pytz.timezone(timezone)
+        # Localize the datetime object to the desired timezone
+        localized_datetime = dt.astimezone(target_timezone)
         return localized_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    except ValueError:
-        return "Invalid epoch value."
+    except (ValueError, pytz.UnknownTimeZoneError) as e:
+        return f"Error: {e}"
