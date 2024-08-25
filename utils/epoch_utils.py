526fe1b2a498ed2c0da479a6e1ec09d54f2885de
nijmehar <nijmehar16@gmail.com>
2024-08-25 15:03:28 +0530

    empty timezone handle

--- utils/epoch_utils.py
+++ utils/epoch_utils.py
@@ -2,14 +2,16 @@ from datetime import datetime
 import pytz
 
 
-def epoch_to_datetime(epoch, timezone='GMT'):
+def epoch_to_datetime(epoch, timezone):
     try:
+        if not timezone:
+            timezone = "GMT"
         # Convert epoch to naive datetime (no timezone info)
         dt = datetime.fromtimestamp(int(epoch))
         # Set the desired timezone
         target_timezone = pytz.timezone(timezone)
         # Localize the datetime object to the desired timezone
         localized_datetime = dt.astimezone(target_timezone)
-        return localized_datetime.strftime('%Y-%m-%d %H:%M:%S')
+        return localized_datetime.strftime('%Y-%m-%d %H:%M:%S') + timezone
     except (ValueError, pytz.UnknownTimeZoneError) as e:
         return f"Error: {e}"
